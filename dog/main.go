1d6192b6e31816a0a1bb3827de1207ffd1c340da
Tristan <tristankrauss123@gmail.com>
2024-10-10 15:00:50 +0200

    changes

---
+++ dog/main.go
@@ -0,0 +1,8 @@
+// Package dog helps convert years.
+package dog
+
+// Returns the converted value when converting human years to dog years
+func convertToDog(i int) int {
+	v := i * 7
+	return v
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+type canine struct {
+	name string
+	age  int
+}
+
+func main() {
+	milo := canine{
+		name: "Milo",
+		age:  dog.convertToDog(15),
+	}
+}
